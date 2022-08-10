c0ee487ee188ce7bf6ec82765857c9077472f554
G. Ferry <grgsferry@gmail.com>
2022-08-10 22:19:53 +0700

    Add player_id to leaderboard response

--- src/domains/games/repository.js
+++ src/domains/games/repository.js
@@ -36,7 +36,7 @@ module.exports = {
     }
 
     const result = await sequelize.query(
-      `SELECT CONCAT(b.first_name, ' ', b.last_name) AS name, b.email, CASE WHEN c.badge_name IS NOT NULL THEN c.badge_name ELSE 'Bronze' END AS badge, SUM(a.points_earned) AS points FROM game_histories a LEFT JOIN users b ON a.player_id = b.id LEFT JOIN user_badges_history c ON a.player_id = c.user_id WHERE a.game_id = ${id} GROUP BY 1,2,3 ORDER BY 4 DESC`
+      `SELECT a.player_id, CONCAT(b.first_name, ' ', b.last_name) AS name, b.email, CASE WHEN c.badge_name IS NOT NULL THEN c.badge_name ELSE 'Bronze' END AS badge, SUM(a.points_earned) AS points FROM game_histories a LEFT JOIN users b ON a.player_id = b.id LEFT JOIN user_badges_history c ON a.player_id = c.user_id WHERE a.game_id = ${id} GROUP BY 1,2,3,4 ORDER BY 5 DESC`
     );
 
     return result[0];
